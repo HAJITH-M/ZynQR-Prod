46d2e4c6828302d802ee487277d60e1802df5476
HAJITH-M <hajithm2003@gmail.com>
2026-05-25 22:58:21 +0530

    fix(auth): user registration if mail send fails then error handling and add user cleanup on failure

--- ZynQR-Server/internal/config/mailConfig/mailConfig.go
+++ ZynQR-Server/internal/config/mailConfig/mailConfig.go
@@ -16,8 +16,7 @@ func LoadSMTPConfig() SmtpConfig {
 		From:     env.AppEnv.SMTP_FROM,
 		Password: env.AppEnv.SMTP_PASSWORD,
 		Host:     env.AppEnv.SMTP_HOST,
-		// Port:     env.AppEnv.SMTP_PORT,
-		Port:     "587",
+		Port:     env.AppEnv.SMTP_PORT,
 		BaseURL:  env.AppEnv.SMTP_URL,
 	}
 }
